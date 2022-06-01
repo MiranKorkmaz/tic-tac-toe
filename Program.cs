5dc66730a4fbc537c43e7eb752f5682bb596ba5e
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-06-01 17:48:02 +0200

    set field template

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tictactoe 
+{
+    internal class Program
+    {
+        // Create play field
+        static void Main(string[] args)
+        {
+            SetField();
+        }
+
+        public static void SetField() {
+            Console.WriteLine("     |      |      ");
+            // TODO replace numbers with variables
+            Console.WriteLine(" {0}   |  {1}   |  {2}", 1,2,3);
+            Console.WriteLine("_____|______|______");
+            Console.WriteLine("     |      |      ");
+            Console.WriteLine(" {0}   |  {1}   |  {2}", 1,2,3);
+            Console.WriteLine("_____|______|______");
+            Console.WriteLine("     |      |      ");
+            Console.WriteLine(" {0}   |  {1}   |  {2}", 1,2,3);
+            Console.WriteLine("_____|______|______");
+            Console.WriteLine("     |      |      ");
+
+        }
+    }
+}
